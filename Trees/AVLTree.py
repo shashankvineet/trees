4751da354bee13f85b534adc361b3377c9f5c850
svineet <svineet@cmc.edu>
2020-04-16 17:11:02 -0700

    added to the AVLTree.py file

--- Trees/AVLTree.py
+++ Trees/AVLTree.py
@@ -6,7 +6,7 @@ The functions in this file are considerably harder than the functions in the Bin
 from Trees.BinaryTree import BinaryTree, Node
 from Trees.BST import BST
 
-class AVLTree():
+class AVLTree(BST):
     '''
     FIXME:
     AVLTree is currently not a subclass of BST.
@@ -19,6 +19,9 @@ class AVLTree():
         FIXME:
         Implement this function.
         '''
+        self.root = None
+        if xs:
+            self.insert_list(xs)
 
 
     def balance_factor(self):
@@ -49,6 +52,9 @@ class AVLTree():
         FIXME:
         Implement this function.
         '''
+        if node is None:
+            return True
+        return AVLTree._balance_factor(node) in [-1,0,1] and AVLTree._is_avl_satisfied(node.left) and AVLTree._is_avl_satisfied(node.right)
 
 
     @staticmethod
@@ -62,6 +68,19 @@ class AVLTree():
         The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
         however, so you will have to adapt their code.
         '''
+        if node is None or node.right is None:
+            return node
+
+        new_node = Node(node.right.value)
+        new_node.right = node.right.right
+
+        new_left = Node(node.value)
+        new_left.left = node.left
+        new_left.right = node.right.left
+
+        new_node.left = new_left
+
+        return new_node
 
 
     @staticmethod
@@ -75,6 +94,18 @@ class AVLTree():
         The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
         however, so you will have to adapt their code.
         '''
+        if node is None or node.left is None:
+            return node
+
+        new_node = Node(node.left.value)
+        new_node.left = node.left.left
+
+        new_right = Node(node.value)
+        new_right.right = node.right
+        new_right.left = node.left.right
+
+        new_node.right = new_right
+        return new_node
 
 
     def insert(self, value):
@@ -92,3 +123,71 @@ class AVLTree():
         The code should look very similar to the code for your insert function for the BST,
         but it will also call the left and right rebalancing functions.
         '''
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self.root = AVLTree._insert(value,self.root)
+
+    def insert_list(self, xs):
+        '''
+        Given a list xs, insert each element of xs into self.
+        FIXME:
+        Implement this function.
+        '''
+
+        for elem in xs:
+            self.insert(elem)
+
+            print(elem, self.root)
+
+    @staticmethod
+    def _rebalance(node):
+
+        if AVLTree._balance_factor(node) > 1:
+
+            if AVLTree._balance_factor(node.left) < 0:
+                node.left=AVLTree._left_rotate(node.left)
+                return AVLTree._right_rotate(node)
+
+            else:
+                return AVLTree._right_rotate(node)
+
+        elif AVLTree._balance_factor(node) < -1:
+
+            if AVLTree._balance_factor(node.right) > 0:
+                node.right=AVLTree._right_rotate(node.right)
+                return AVLTree._left_rotate(node)
+
+            else:
+                return AVLTree._left_rotate(node)
+        else:
+
+            return node
+
+
+    @staticmethod
+    def _insert(value,node):
+
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                AVLTree._insert(value, node.left)
+        elif value > node.value:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                AVLTree._insert(value, node.right)
+        else:
+            print("value is already present in tree")
+
+
+        if not AVLTree._is_avl_satisfied(node):
+            node.left = AVLTree._rebalance(node.left)
+            node.right = AVLTree._rebalance(node.right)
+            return AVLTree._rebalance(node)
+        else:
+            return node
+
+
+print("complete")
